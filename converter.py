718fbd61edba7c339f1d89b6b71f5355edf6545d
zachary <syyun@snu.ac.kr>
2020-01-31 17:51:33 +0900

    Add fpath f. test-data

--- converter.py
+++ converter.py
@@ -1,11 +1,13 @@
 import scipy.io
 import numpy as np
 
-data = scipy.io.loadmat("subject.mat")
+fpath_test_data = './burgers_shock.mat'
+
+data = scipy.io.loadmat(fpath_test_data)
 
 for i in data:
 	if '__' not in i and 'readme' not in i:
-		np.savetxt(("filesforyou/"+i+".csv"),data[i],delimiter=',')
+		np.savetxt(("filesforyou/"+i+".csv"), data[i], delimiter=',')
 
 if __name__ == "__main__":
     pass
